4461adc8270a11f441d48497fbada92cd6d8cb53
1kivanc <kivanc0556@gmail.com>
2024-06-16 12:12:28 +0300

    book model

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,40 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 const app  = express();
+
+app.use(express.json());
 
 app.get('/',(req,res) => {
     console.log(req);
     return res.status(234).send('Welcome to mern stack Tutorial');
 })
 
+app.post("/books",async(req,res) => {
+    try{
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear 
+        ){
+            return res.status(400).send({
+                message:'send all required fields: title,author,publishYear'
+            });
 
+        }
+        const newBook = {
+            title:req.body.title,
+            author:req.body.author,
+            publishYear:req.body.publishYear
+        };
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send({message:err.message});
+    }
+});
 
 mongoose
 .connect(mongoDBURL)
