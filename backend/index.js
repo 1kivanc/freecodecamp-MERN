a5e1acc45d2daa908e71ab823a9e019a76bb12dd
1kivanc <kivanc0556@gmail.com>
2024-06-16 10:29:01 +0300

    first http route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import { PORT } from "./config.js";
 const app  = express();
 
+app.get('/',(req,res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to mern stack Tutorial');
+})
+
 app.listen(PORT,() => {
     console.log(`App listen to port: ${PORT}`);
 })
