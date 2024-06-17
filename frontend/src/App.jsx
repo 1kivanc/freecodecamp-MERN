631da5be3d02ed74351343a0a4e90d8fd7c1b1fe
1kivanc <kivanc0556@gmail.com>
2024-06-17 09:55:17 +0300

    show book details in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBook /> } />
-      <Route path='/books/details/:id' element={<showBook />} />
-      <Route path='/books/edit/:id' element={<editBook />} />
-      <Route path='books/delete/:id' element={<deleteBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='books/delete/:id' element={<DeleteBook />} />
 
     </Routes>
   )
